e7ddffb24b454748f33eb3d81671f8b276e31dde
leeys <leeys.1@gmail.com>
2017-08-05 18:40:08 +0800

    Fix module registering

--- reconnecting-websocket.js
+++ reconnecting-websocket.js
@@ -93,13 +93,7 @@
  *
  */
 (function (global, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else if (typeof module !== 'undefined' && module.exports){
-        module.exports = factory();
-    } else {
-        global.ReconnectingWebSocket = factory();
-    }
+    exports.default = factory();
 })(this, function () {
 
     if (!('WebSocket' in window)) {
